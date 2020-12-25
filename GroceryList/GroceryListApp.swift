8db9dbdd967e739b76630e5226537cc1edeacf96
Darrien Huntley <info@dhuntleyinc.com>
2020-12-25 02:46:46 -0500

    Initial Commit

---
+++ GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by Darrien Huntley on 12/25/20.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
